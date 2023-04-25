a938948fdc9675e91061aedc6131d35522f702b0
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-04-26 00:36:10 +0800

    writing codes for each pseudocodes
    Removed #pseudocode at line 1. Rewrite the pseudocode/comment for #parse & #split at line3-4.

--- number_parser.py
+++ number_parser.py
@@ -1,8 +1,29 @@
-# PSEUDOCODE
 # Open the text file "numbers.txt" in read mode
-# Parse and split each line of integers
-# Split integers into list
+with open("numbers.txt", "r") as num_file:
+    content = num_file.read()
+
+#split content into list of integers
+numbers = [int(i) for i in content.split()]
+
 # Create two empty lists to store: odd and even integers
+odd_int = []
+even_int = []
+
 # Check and sort each integer into the two empty lists
-# Open text file "odd.txt" to write odd integers
-# Open text file "even.txt" to write even integers
+for num in numbers:
+    if num % 2 == 0:
+        even_int.append(str(num))
+    else:
+        odd_int.append(str(num))
+
+# Create a text file "odd.txt" to write even integers
+with open("even.txt", "w") as f:
+    num_file.write("EVEN INTEGERS\n")
+    for num in even_int:
+        num_file.write(str(num) + "\n")
+
+# Create a text file "even.txt" to write odd integers
+with open("odd.txt", "w") as f:
+    num_file.write("ODD INTEGERS\n")
+    for num in odd_int:
+        num_file.write(str(num) + "\n")
