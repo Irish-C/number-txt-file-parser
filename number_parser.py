bc972247bc52155376111aa7c849066bab0dd14a
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-04-25 22:13:15 +0800

    pseudocode

---
+++ number_parser.py
@@ -0,0 +1,8 @@
+# PSEUDOCODE
+# Open the text file "numbers.txt" in read mode
+# Parse and split each line of integers
+# Split integers into list
+# Create two empty lists to store: odd and even integers
+# Check and sort each integer into the two empty lists
+# Open text file "odd.txt" to write odd integers
+# Open text file "even.txt" to write even integers
